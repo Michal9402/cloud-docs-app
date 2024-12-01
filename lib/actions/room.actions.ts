ad18f08f7fe80d352c55ffd41db1d061f833f98f
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-01 15:14:42 +0100

    add documents list

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -50,6 +50,16 @@ export const getDocument = async ({ roomId, userId }: { roomId: string; userId: 
   }
 }
 
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting rooms: ${error}`);
+  }
+}
+
 export const updateDocument = async (roomId: string, title: string) => {
   try {
     const updatedRoom = await liveblocks.updateRoom(roomId, {
